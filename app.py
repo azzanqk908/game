11bb75455919f61792cee8f9dc12dd7c5f420b4b
Azzan <azzan.kindy@gmail.com>
2025-03-16 20:37:17 +0400

    update 0.1.42

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 from threading import Lock
 import time, os
